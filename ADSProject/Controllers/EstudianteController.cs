d8c9de222412c4db43cbc14008b79aefeeba8227
Gabriel Munguia <juayua2011@gmail.com>
2022-03-19 14:53:25 +0100

    Se agregar  cambios en estudiante controller

--- ADSProject/Controllers/EstudianteController.cs
+++ ADSProject/Controllers/EstudianteController.cs
@@ -1,4 +1,6 @@
-﻿using ADSProject.Repository;
+﻿using ADSProject.Models;
+using ADSProject.Repository;
+using ADSProject.Utils;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -18,9 +20,10 @@ namespace ADSProject.Controllers
             this.estudianteRepository = estudianteRepository;
         }
 
-
+        [HttpGet]
         public IActionResult Index()
         {
+
             try
             {
                 var item = estudianteRepository.obtenerEstudiantes();
@@ -33,6 +36,65 @@ namespace ADSProject.Controllers
                 throw;
             }
         }
-           
+
+
+        [HttpGet]
+        public IActionResult Form  (int? idEstudiante,Operaciones operaciones)   {
+            try
+            {
+                var estudiante = new EstudianteViewModel();
+                if (idEstudiante.HasValue) {
+                    estudiante = estudianteRepository.obtenerEstudiantePorID(idEstudiante.Value);
+                }
+                ViewData["Operaciones"] = operaciones;
+                return View(estudiante);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        [HttpPost]
+        public  IActionResult Form (EstudianteViewModel estudianteViewModel)
+        {
+            try
+            {
+                if (estudianteViewModel.idEstudiante == 0)
+                {
+                    estudianteRepository.agregarEstudiante(estudianteViewModel);
+                }
+                else
+                {
+                    estudianteRepository.actualizarEstudiante(estudianteViewModel.idEstudiante, estudianteViewModel);
+                }
+
+                return RedirectToAction("Index");
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        [HttpGet]
+        public IActionResult Delete (int idEstudiante)
+        {
+            try
+            {
+                estudianteRepository.eliminarEstudiante(idEstudiante);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            return RedirectToAction("Index");
+        }
+
+
+
+
     }
 }
